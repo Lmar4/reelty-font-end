757d52d67c26b2714655c5cb3ec70a87fd7f6351
Antonio Correa <antonio.correa@gmail.com>
2025-02-06 18:23:58 -0300

    Add platform configuration, Elastic Beanstalk files, Procfile

--- lib/trpc.ts
+++ lib/trpc.ts
@@ -6,9 +6,8 @@ import { QueryClient } from "@tanstack/react-query";
 import { getAuth } from "firebase/auth";
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 4000}`; // dev SSR should use localhost
+  // Use the environment variable for the API base URL
+  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000';
 };
 
 export const trpc = createTRPCReact<AppRouter>();
