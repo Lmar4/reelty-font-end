98e419ee31aabccf397d671cf09b0ca79c8a39e2
Lmar4 <lucasmartinbuilding@gmail.com>
2025-03-05 13:18:19 -0800

    New Beta Pricing with Gradient

--- components/reelty/FreeTrial.tsx
+++ components/reelty/FreeTrial.tsx
@@ -1,38 +1,28 @@
 "use client";
 
-import { useState } from "react";
-import PricingModal from "./PricingModal";
+import { useRouter } from "next/navigation";
 
 export default function FreeTrial() {
-  const [isUpgradeModalOpen, setIsUpgradeModalOpen] = useState(false);
+  const router = useRouter();
 
   return (
-    <>
-      <div className='w-full'>
-        <div className='bg-[#1c1c1c] text-white px-4 py-2 text-[14px] flex items-center justify-between'>
-          <div>
-            <span className='text-white font-semibold'>
-              You're in a free trial.
-            </span>{" "}
-            <span className='text-white/40'>
-              Some features are limited. Upgrade to unlock.
-            </span>
-          </div>
-          <button
-            onClick={() => setIsUpgradeModalOpen(true)}
-            className='bg-white text-black px-4 py-1 rounded-lg text-[15px] font-semibold'
-          >
-            Upgrade
-          </button>
+    <div className='w-full'>
+      <div className='bg-[#1c1c1c] text-white px-4 py-2 text-[14px] flex items-center justify-between'>
+        <div>
+          <span className='text-white font-semibold'>
+            You're in a free trial.
+          </span>{" "}
+          <span className='text-white/40'>
+            Some features are limited. Upgrade to unlock.
+          </span>
         </div>
+        <button
+          onClick={() => router.push("/pricing")}
+          className='bg-white text-black px-4 py-1 rounded-lg text-[15px] font-semibold'
+        >
+          Upgrade
+        </button>
       </div>
-
-      <PricingModal
-        isOpen={isUpgradeModalOpen}
-        onClose={() => setIsUpgradeModalOpen(false)}
-        listingId=''
-        onUpgradeComplete={() => setIsUpgradeModalOpen(false)}
-      />
-    </>
+    </div>
   );
 }
